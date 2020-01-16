3c9e58969423a30b13ab89ef2b3b72f9744c25ee
jmcasebier <jmcasebier@acm.org>
2020-01-16 12:17:53 -0600

    added CometChat initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CometChat } from '@cometchat-pro/chat';
+import { APP_ID } from './config.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    CometChat.init(APP_ID).then(
+      hasInitialized => {
+        console.log("Initialization completed successfully", hasInitialized);
+      },
+      error => {
+        console.log("Initialization failed with error:", error);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const APP_ID = "13179c4949950ab";
+const API_KEY = "805eaa3f4691b61ab1466bbee2121ce26ee7811d";
+export { APP_ID, API_KEY };
