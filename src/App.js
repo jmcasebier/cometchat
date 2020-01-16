07976aed5a37ba28d0659b2f9e7f670c0477e61f
jmcasebier <jmcasebier@acm.org>
2020-01-16 12:55:38 -0600

    working login component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import { CometChat } from '@cometchat-pro/chat';
 import { APP_ID } from './config.js';
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     
     CometChat.init(APP_ID).then(
       hasInitialized => {
-        console.log("Initialization completed successfully", hasInitialized);
+        console.log("Initialization completed successfully:", hasInitialized);
       },
       error => {
         console.log("Initialization failed with error:", error);
@@ -25,5 +25,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { CometChat } from '@cometchat-pro/chat';
+import { API_KEY } from '../../config.js';
+import './index.css';
+import Loading from './loading.svg';
+
+export default class Login extends Component {
+    
+}
